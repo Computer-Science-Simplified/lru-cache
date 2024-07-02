7389c3971dc6f1f8e620ae54051373039e1eb327
martinjoo <martin@martinjoo.dev>
2024-07-02 14:46:21 +0200

    Implement LRU with Redis sorted sets

---
+++ app/Cache/LRUCacheRedisSet.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Cache;
+
+use Illuminate\Support\Facades\Redis;
+
+class LRUCacheRedisSet extends LRUCache
+{
+    private string $setKey;
+
+    private string $mapKey;
+
+    public function __construct(int $capacity, string $setKey, string $mapKey)
+    {
+        parent::__construct($capacity);
+
+        $this->setKey = $setKey;
+
+        $this->mapKey = $mapKey;
+    }
+
+    public function put(string $key, mixed $value): void
+    {
+        if (Redis::zcard($this->setKey) >= $this->capacity) {
+            $result = Redis::zpopmin($this->setKey);
+
+            $keys = array_keys($result);
+
+            Redis::hdel($this->mapKey, $keys[0]);
+        }
+
+        Redis::zadd($this->setKey, now()->getPreciseTimestamp(), $key);
+
+        Redis::hset($this->mapKey, $key, $value);
+    }
+
+    public function get(string $key): mixed
+    {
+        $value = Redis::hget($this->mapKey, $key);
+
+        if (!$value) {
+            return null;
+        }
+
+        Redis::zadd($this->setKey, now()->getPreciseTimestamp(), $key);
+
+        return $value;
+    }
+}
---
+++ tests/Unit/LRUCacheRedisSetTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Cache\LRUCacheRedisList;
+use App\Cache\LRUCacheRedisSet;
+use Illuminate\Support\Facades\Redis;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class LRUCacheRedisSetTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Redis::flushall();
+    }
+
+    #[Test]
+    public function it_should_keep_elements_in_the_right_order()
+    {
+        $lru = new LRUCacheRedisSet(2, 'items', 'cache');
+
+        $lru->put('first', 1);
+        $lru->put('second', 2);
+
+        $this->assertSame(1, (int) $lru->get('first'));
+
+        $lru->put('third', 3);
+
+        $this->assertNull($lru->get('second'));
+
+        $lru->put('fourth', 4);
+
+        $this->assertNull($lru->get('first'));
+        $this->assertSame(3, (int) $lru->get('third'));
+        $this->assertSame(4, (int) $lru->get('fourth'));
+    }
+}
