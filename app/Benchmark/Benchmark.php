0e21f47f5fdd10a215d20cd6595eea8317621798
martinjoo <martin@martinjoo.dev>
2024-07-02 14:46:28 +0200

    Add sorted set to benchmark

--- app/Benchmark/Benchmark.php
+++ app/Benchmark/Benchmark.php
@@ -6,6 +6,8 @@ use App\Cache\LRUCache;
 use App\Cache\LRUCacheArray;
 use App\Cache\LRUCacheLinkedList;
 use App\Cache\LRUCacheRedisList;
+use App\Cache\LRUCacheRedisSet;
+use Illuminate\Support\Facades\Redis;
 
 class Benchmark
 {
@@ -13,6 +15,8 @@ class Benchmark
     {
         ini_set('memory_limit', -1);
         ini_set('max_execution_time', -1);
+
+        Redis::flushall();
 
         $benchmark = function (LRUCache $cache, int $n) {
             foreach (range(1, $n) as $i) {
@@ -24,27 +28,31 @@ class Benchmark
             }
         };
 
-        $result = \Illuminate\Support\Benchmark::measure([
-            'array_1000' => function () use ($benchmark) {
+        \Illuminate\Support\Benchmark::dd([
+            'array:1_000' => function () use ($benchmark) {
                 $benchmark(new LRUCacheArray(1_000), 1_000);
             },
-            'array_10_000' => function () use ($benchmark) {
+            'array:10_000' => function () use ($benchmark) {
                 $benchmark(new LRUCacheArray(10_000), 10_000);
             },
-            'linked_list_1000' => function () use ($benchmark) {
+            'linked_list:1_000' => function () use ($benchmark) {
                 $benchmark(new LRUCacheLinkedList(1_000), 1_000);
             },
-            'linked_list_10_000' => function () use ($benchmark) {
+            'linked_list:10_000' => function () use ($benchmark) {
                 $benchmark(new LRUCacheLinkedList(10_000), 10_000);
             },
-            'redis_1000' => function () use ($benchmark) {
-                $benchmark(new LRUCacheRedisList(1_000, 'items', 'cache'), 1_000);
+            'redis_set:1_000' => function () use ($benchmark) {
+                $benchmark(new LRUCacheRedisSet(1_000, 'items_set', 'cache_set'), 1_000);
             },
-            'redis_10_000' => function () use ($benchmark) {
-                $benchmark(new LRUCacheRedisList(10_000, 'items', 'cache'), 10_000);
+            'redis_set:10_000' => function () use ($benchmark) {
+                $benchmark(new LRUCacheRedisSet(10_000, 'items_set', 'cache_set'), 10_000);
+            },
+            'redis_list:1_000' => function () use ($benchmark) {
+                $benchmark(new LRUCacheRedisList(1_000, 'items_list', 'cache_list'), 1_000);
+            },
+            'redis_list:10_000' => function () use ($benchmark) {
+                $benchmark(new LRUCacheRedisList(10_000, 'items_list', 'cache_list'), 10_000);
             },
         ]);
-
-        dd($result);
     }
 }
